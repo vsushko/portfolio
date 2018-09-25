73c254c9861f2aa9015b3b23b235db3a535b0b75
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-09-25 22:36:53 +0300

    renamed package

--- maxcom/README.md
+++
@@ -1,9 +0,0 @@
-# Тестовое задание для Java
-Написать примитивный mtproto сервер с помощью `java.nio.ByteBuffer` (для парсеров сообщений) и NIO2. Сервер должен уметь выполнять первые 2 команды при инициализации DH-сессии: *req_pq* и *req_DH_params*. На *req_pq* отдает *res_pq* со случайными данными и ждет *req_DH_params* на который отвечает *server_DH_params_ok* и закрывает коннект.
-
-Документация по mtproto:
-
-* https://core.telegram.org/mtproto/auth_key
-* https://core.telegram.org/mtproto/description#unencrypted-message
-
-Тестовый проект загружаете на свой github и в комментариях тут пишите ссылку.
---
+++ mtproto-server/src/main/java/ru/maxcom/AppUtils.java
@@ -0,0 +1,38 @@
+package ru.maxcom;
+
+import java.util.Random;
+
+/**
+ * App utils
+ *
+ * @author vsushko
+ */
+public final class AppUtils {
+
+    /**
+     * Port
+     */
+    public static final int PORT = 1113;
+
+    /**
+     * Host name
+     */
+    public static final String HOST_NAME = "localhost";
+
+    /**
+     * Use only allowed methods
+     */
+    private AppUtils() {
+        super();
+    }
+
+    /**
+     * Returns random 6-digit number
+     *
+     * @return number
+     */
+    public static int generateRandomNumber() {
+        Random random = new Random();
+        return 100000 + random.nextInt(900000);
+    }
+}
