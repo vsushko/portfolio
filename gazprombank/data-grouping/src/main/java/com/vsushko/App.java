2c83625a784b89855d151117c176150ff4680cae
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-10-27 22:02:42 +0300

    added new file

--- gazprombank/data-grouping/src/main/java/com/vsushko/App.java
+++ gazprombank/data-grouping/src/main/java/com/vsushko/App.java
@@ -1,13 +1,20 @@
 package com.vsushko;
 
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        Map<Integer, Integer> map = new TreeMap<>();
+        Set<Integer> set = new TreeSet<>();
+        Queue<Integer> queue = new PriorityQueue<>();
     }
 }
